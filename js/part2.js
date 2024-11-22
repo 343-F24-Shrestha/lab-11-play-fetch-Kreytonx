83aa92b1bcff7e2d62ae0bc04f4e7dab40b84ab0
Kreytonx <107674836+Kreytonx@users.noreply.github.com>
2024-11-21 21:16:42 -0500

    part2

--- js/part2.js
+++ js/part2.js
@@ -7,4 +7,29 @@ document.addEventListener("DOMContentLoaded", async () => {
     // The format should follow the example user in the HTML file.
 
     // TODO
+    const userList = document.getElementById("user-list");
+
+    try{
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const data = await response.json();
+
+        data.data.forEach(user => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            const name = document.createElement("h2");
+            name.textContent = `${user.first_name} ${user.last_name}`;
+            card.appendChild(name);
+
+            const avatar = document.createElement("img");
+            avatar.src = user.avatar;
+            avatar.alt = `${user.first_name} ${user.last_name}`;
+            card.appendChild(avatar);
+
+            userList.appendChild(card);
+        });
+    } 
+    catch (error){
+        console.error("Error fetching users:", error);
+    }
 });
